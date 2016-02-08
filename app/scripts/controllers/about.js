7f4638361846edf44a68ecda47ba5cff92010f46
Aaron Turner <aaron@aaronthedev.com>
2016-02-08 14:26:21 -0800

    Generated the Angular Project
    Generated the angular project with Yeoman, and changed the readme
    to the yeoman default

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name signupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the signupApp
+ */
+angular.module('signupApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
