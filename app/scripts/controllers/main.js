44f6bd677e2677b57d8e71b136e54ad26a8233d0
Aaron Turner <aaron@aaronthedev.com>
2016-02-16 19:15:35 -0800

    Added basic Html and logic
    Added the basic HTML as well as some javascript to validate our email

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,33 @@
  */
 angular.module('signupApp')
   .controller('MainCtrl', function () {
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    //Initialize our form data
+    $scope.formData = {};
+    //Variable for if we have a valid email
+    $scope.validEmail = false;
+
+    /**
+     * Regex to Validate email field
+     */
+    $scope.validateEmail = function() {
+        //Fetch email from giftcard form
+        var email = $scope.formData.email;
+
+        //Regex for all valid emails. To add a TLD, edit the final OR statement.
+        var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|co|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/;
+        //Test the form email against the regex
+        if (emailRegex.test(email)) {
+            $scope.validEmail = true;
+        } else {
+            $scope.validEmail = false;
+        }
+    }
+
+});
