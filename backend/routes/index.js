74a37c944a1a29c1d4f034a077a6a9fdef604289
Aaron Turner <aaron@aaronthedev.com>
2016-03-05 01:31:16 -0800

    Generated backend, Added an ifttt key check
    as the title sates,doing this for secure ifttt keys.Also, git ignored node modules in the backend

---
+++ backend/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+//Using request for super simple api requests
+var request = require('request');
+
+//Our api key
+//Do not add this until build
+var iftttKey = "";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//Post to ifttt
+router.post('/sheets', function(req, res, next) {
+
+    //Prepare our object
+    //Value 1 = Atendee name
+    //Value 2 = Atendee email
+    //Value 3 = Atendee Org Opt-in
+    var iftttPayload = {
+        "value1": req.body.name,
+        "value2": req.body.email,
+        "value3": req.body.orgs
+    }
+
+    //Make the request
+    request.post(
+        'https://maker.ifttt.com/trigger/sheets/with/key/' + iftttKey,
+        { form: iftttPayload },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+        }
+    );
+});
+
+module.exports = router;
